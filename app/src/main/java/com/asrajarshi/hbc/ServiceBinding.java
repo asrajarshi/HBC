a8991cbc1dc0537e948bcac14149523098fa4325
asrajarshi <asr471@nyu.edu>
2015-10-24 11:00:18 -0400

    added second activity

--- app/src/main/java/com/asrajarshi/hbc/ServiceBinding.java
+++ app/src/main/java/com/asrajarshi/hbc/ServiceBinding.java
@@ -11,50 +11,51 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.mbientlab.metawear.MetaWearBleService;
 import com.mbientlab.metawear.MetaWearBoard;
 
-public class MainActivity extends AppCompatActivity {
+public class ServiceBinding extends AppCompatActivity implements ServiceConnection{
+    private MetaWearBleService.LocalBinder serviceBinder;
+    private final String MW_MAC_ADDRESS= "EC:2C:09:81:22:AC"; // put MAC address of our device here
+    private MetaWearBoard mwBoard;
+    TextView sample;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_service_binding);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getApplicationContext().bindService(new Intent(this, MetaWearBleService.class), this, Context.BIND_AUTO_CREATE);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,ServiceBinding.class);
-                startActivity(intent);
-                finish();
+                sample = (TextView) view.findViewById(R.id.sample);
+                sample.setText("you have pressed the button");
             }
         });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onServiceConnected(ComponentName name, IBinder service) {
+        serviceBinder = (MetaWearBleService.LocalBinder) service;
+
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onServiceDisconnected(ComponentName name) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        // Unbind the service when the activity is destroyed
+        getApplicationContext().unbindService(this);
+    }
+
 }
